9d3901d12126cfdc250d4c7ff6b28c6440a97022
gabriel <gabrielmesquita@cjr.org.br>
2017-01-16 17:44:53 -0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+
+	render() {
+		return (
+			<Text>Teste</Text>
+		);
+	}
+
+}
+
+export default App;
