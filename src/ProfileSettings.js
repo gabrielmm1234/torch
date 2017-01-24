356c6e3fa7fe4f8837b8e76ebcddc818400985c6
renanreboredor3 <renanreboredo@paramag.com.br>
2017-01-24 17:40:09 -0200

    Adding the component ProfileItemContainer to ProfileSettings.js

--- src/ProfileSettings.js
+++ src/ProfileSettings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { 
-	View, 
+import {
+	View,
 	Text,
 	Image,
 	TouchableOpacity
@@ -45,61 +45,61 @@ class ProfileSettings extends Component {
 					</TouchableOpacity>
 
 					<Icon name="user-circle" size={25} color="white" />
-					<Text style={styles.profileSettingsTextStyle}> 
+					<Text style={styles.profileSettingsTextStyle}>
 						PROFILE SETTINGS
 					</Text>
 				</View>
 
 				<View style={styles.profileContainer}>
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>NAME</Text>
 						<Text style={[styles.textStyle, {fontSize: 40}]}>Gabriel Mesquita</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>NICKNAME</Text>
 						<Text style={[styles.profileItemStyle, {color: 'grey', fontSize: 15}]}>Add A Nickname</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>LOCATION</Text>
 						<Text style={[styles.profileItemStyle, {fontSize: 20}]}>Portland, OR USA</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>TORCH ID</Text>
 						<Text style={[styles.profileItemStyle, {fontSize: 20}]}>@ripcityrebels</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>MOBILE</Text>
 						<Text style={[styles.profileItemStyle, {fontSize: 20}]}>1 555 666 7777</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>EMAIL</Text>
 						<Text style={[styles.profileItemStyle, {fontSize: 20}]}>gabrieljustware@gmail.com</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>BIRTHDAY</Text>
 						<Text style={[styles.profileItemStyle, {fontSize: 20}]}>04 january, 1996</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>Gender</Text>
 						<Text style={[styles.profileItemStyle, {color: 'grey', fontSize: 15}]}>Select</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>Height</Text>
 						<Text style={[styles.profileItemStyle, {fontSize: 20}]}>6'2"</Text>
-					</View>
+					</ProfileItemContainer>
 
-					<View style={styles.profileItemContainer}>
+					<ProfileItemContainer>
 						<Text style={styles.profileLabelStyle}>Weight</Text>
 						<Text style={[styles.profileItemStyle, {fontSize: 20}]}>210 lbs</Text>
-					</View>
+					</ProfileItemContainer>
 				</View>
 
 			</Image>
