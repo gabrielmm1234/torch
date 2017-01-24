310ff089faadbf426379b575093bc61d9cb56ef3
renanreboredor3 <renanreboredo@paramag.com.br>
2017-01-24 17:33:22 -0200

    Adding component ProfileItemContainer to the project

---
+++ src/components/ProfileItemContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+import styles from './styles';
+
+class ProfileItemContainer extends Component {
+  <View style={style.profileItemContainer} />
+}
+
+export default ProfileItemContainer;
