770497b467a48c204c83106d37895b7886be90dd
renanreboredor3 <renanreboredo@paramag.com.br>
2017-01-24 17:49:17 -0200

    Adding component ProfileLabel and minor changes to ProfileItemContainer component

--- src/components/ProfileItemContainer.js
+++ src/components/ProfileItemContainer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
 } from 'react-native';
 import styles from './styles';
 
 class ProfileItemContainer extends Component {
-  <View style={style.profileItemContainer} />
+  render(){
+    <View style={style.profileItemContainer} />
+  }
 }
 
 export default ProfileItemContainer;
---
+++ src/components/ProfileLabel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
+import styles from './styles';
+
+class ProfileLabel extends Component {
+  render(){
+    <Text style={styles.profileLabelStyle} />
+  }
+}
+
+export default ProfileLabel;
